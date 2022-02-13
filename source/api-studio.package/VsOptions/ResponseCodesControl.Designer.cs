2fcd62178ce3f4c495997c2229a089a3740d52f5
butsona <butsona@arbs.io>
2022-02-13 22:13:20 +0100

    removed unused controls

--- source/api-studio.package/VsOptions/ResponseCodesControl.Designer.cs
+++ source/api-studio.package/VsOptions/ResponseCodesControl.Designer.cs
@@ -30,43 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResponseCodesControl));
-            this.chkShowSelected = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Search = new System.Windows.Forms.Label();
             this.lvwResponseCodes = new System.Windows.Forms.ListView();
             this.Code = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ControlImages = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
-            // 
-            // chkShowSelected
-            // 
-            this.chkShowSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.chkShowSelected.AutoSize = true;
-            this.chkShowSelected.Location = new System.Drawing.Point(261, 12);
-            this.chkShowSelected.Name = "chkShowSelected";
-            this.chkShowSelected.Size = new System.Drawing.Size(98, 17);
-            this.chkShowSelected.TabIndex = 1;
-            this.chkShowSelected.Text = "Show Selected";
-            this.chkShowSelected.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(58, 10);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // Search
-            // 
-            this.Search.AutoSize = true;
-            this.Search.Location = new System.Drawing.Point(13, 14);
-            this.Search.Name = "Search";
-            this.Search.Size = new System.Drawing.Size(44, 13);
-            this.Search.TabIndex = 3;
-            this.Search.Text = "Search:";
             // 
             // lvwResponseCodes
             // 
@@ -79,11 +47,11 @@
             this.Description});
             this.lvwResponseCodes.FullRowSelect = true;
             this.lvwResponseCodes.HideSelection = false;
-            this.lvwResponseCodes.Location = new System.Drawing.Point(16, 36);
+            this.lvwResponseCodes.Location = new System.Drawing.Point(16, 14);
             this.lvwResponseCodes.MultiSelect = false;
             this.lvwResponseCodes.Name = "lvwResponseCodes";
             this.lvwResponseCodes.ShowItemToolTips = true;
-            this.lvwResponseCodes.Size = new System.Drawing.Size(343, 209);
+            this.lvwResponseCodes.Size = new System.Drawing.Size(343, 231);
             this.lvwResponseCodes.TabIndex = 4;
             this.lvwResponseCodes.UseCompatibleStateImageBehavior = false;
             this.lvwResponseCodes.View = System.Windows.Forms.View.Details;
@@ -111,20 +79,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lvwResponseCodes);
-            this.Controls.Add(this.Search);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.chkShowSelected);
             this.Name = "ResponseCodesControl";
             this.Size = new System.Drawing.Size(371, 260);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.CheckBox chkShowSelected;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label Search;
         private System.Windows.Forms.ListView lvwResponseCodes;
         private System.Windows.Forms.ColumnHeader Code;
         private System.Windows.Forms.ColumnHeader Description;
