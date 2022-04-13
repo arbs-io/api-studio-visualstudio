bebd9b26dc0be7358f472ced09e5288f0d3898da
butsona <butsona@arbs.io>
2022-04-13 16:34:38 +0200

    Bug in Microsoft.Azure...OpenApi 1.3.0 >> 1.2.0

--- templates/api-studio.azure-function-dotnet6-isolated/AzFunction/Models/ErrorModel.cs
+++ templates/api-studio.azure-function-dotnet6-isolated/AzFunction/Models/ErrorModel.cs
@@ -8,7 +8,7 @@ namespace Models
         [OpenApiProperty(Description = "Machine-friendly error code, which could be used in error handling", Default = 404, Nullable = false)]
         public int statusCode { get; set; } = 200;
 
-        [OpenApiProperty(Description = "Human-readable error description.", Default = "Resource not found", Nullable = false)]
+        [OpenApiProperty(Description = "Human-readable error description.", Default = "Useful information about the failure e.g. Resource not found", Nullable = false)]
         public string? Message { get; set; } = "";
     }
 }
