c7184afc99c8e79d5fd8c0bddfae9f58dcb3804f
arbs-io <71968891+arbs-io@users.noreply.github.com>
2022-07-31 22:11:54 +0200

    adding api-studio.codegen.csharp-minimumapi-dotnet6 (#33)

--- vs-templates/package-templates.ps1
+++ vs-templates/package-templates.ps1
@@ -13,3 +13,13 @@ if (Test-Path $template_path) {
 }
 Write-Host "Packing: $($template_path)" -ForegroundColor Green
 Compress-Archive -Path .\api-studio.codegen.csharp-azurefunction-dotnet6\* -DestinationPath $template_path
+
+
+# Build api-studio.codegen.csharp-azurefunction-dotnet6
+$template_path = "..\src\api-studio.package\ProjectTemplates\csharp-minimumapi-dotnet6.zip"
+if (Test-Path $template_path) {
+    Write-Host "Removing: $($template_path)" -ForegroundColor DarkMagenta
+    Remove-Item $template_path
+}
+Write-Host "Packing: $($template_path)" -ForegroundColor Green
+Compress-Archive -Path .\api-studio.codegen.csharp-minimumapi-dotnet6\* -DestinationPath $template_path
