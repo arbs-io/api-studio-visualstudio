c44c8dd6cef58b6774daa4795f48908cbfefbfed
Andrew Butson <butsona@arbs.io>
2022-08-14 22:56:54 +0200

    working code gen using dsl

--- src/api-studio.codegen.csharp-minimalapi-dotnet6/Build/SdkHttpEndpointDesigner.cs
+++ src/api-studio.codegen.csharp-minimalapi-dotnet6/Build/SdkHttpEndpointDesigner.cs
@@ -3,14 +3,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Net;
-using System.Text;
 using ApiStudioIO.Common.Models.Build;
-using ApiStudioIO.Common.Models.Http;
 using ApiStudioIO.Utility.Extensions;
-using ApiStudioIO.Vs.Options;
 using ApiStudioIO.Vs.Output;
 
 namespace ApiStudioIO.CodeGen.CSharpMinimalApiDotNet6.Build
@@ -43,22 +38,22 @@ namespace ApiStudioIO.CodeGen.CSharpMinimalApiDotNet6.Build
             _ = resource ?? throw new ArgumentNullException(nameof(resource));
             _ = httpApi ?? throw new ArgumentNullException(nameof(httpApi));
 
-            var attributes = new List<string>();
-            attributes.AddRange(BuildHttpTriggerParameters(httpApi));
-            attributes.AddRange(BuildHttpTriggerResponseStatusCodes(httpApi));
-            var openapiAttributes = string.Join(Environment.NewLine, attributes);
+            //var attributes = new List<string>();
+            //attributes.AddRange(BuildHttpTriggerParameters(httpApi));
+            ////attributes.AddRange(BuildHttpEndpointResponseStatusCodes(httpApi));
+            //var openapiAttributes = string.Join(Environment.NewLine, attributes);
+            var httpEndpointResponses = BuildHttpEndpointResponseStatusCodes(httpApi);
 
             var httpTriggerDesignerSourceCode = Templates.MinimalApiResource.HttpEndpointDesigner
-                .Replace("{{TOKEN_OAS_PROJECTNAME}}", buildTargetModel.ProjectName)
+                .Replace("{{TOKEN_OAS_PROJECT_NAME}}", buildTargetModel.ProjectName)
                 .Replace("{{TOKEN_OAS_NAMESPACE}}", namespaceHelper.Solution)
-                .Replace("{{TOKEN_OAS_MODEL}}", modelName)
-                .Replace("{{TOKEN_OAS_NAMESPACE_DATAMODEL}}", namespaceHelper.DataModel)
                 .Replace("{{TOKEN_OAS_CLASS_NAME}}", $"Http{httpApi.DisplayName.ToAlphaNumeric()}")
-                .Replace("{{TOKEN_OAS_FUNCTION_NAME}}", httpApi.DisplayName.ToAlphaNumeric())
-                .Replace("{{TOKEN_OAS_FUNCTION_DESCRIPTION}}", httpApi.Description)
                 .Replace("{{TOKEN_OAS_HTTP_VERB}}", httpApi.HttpVerb)
                 .Replace("{{TOKEN_OAS_HTTP_URI}}", resource.HttpApiUri)
-                .Replace("{{TOKEN_OAS_HTTP_OPENAPI_ATTRIBUTE}}", openapiAttributes);
+                .Replace("{{TOKEN_OAS_HTTP_TAG}}", $"{modelName}")
+                .Replace("{{TOKEN_OAS_HTTP_NAME}}", $"{httpApi.DisplayName}")
+                .Replace("{{TOKEN_OAS_DESCRIPTION}}", httpApi.Description)
+                .Replace("{{TOKEN_OAS_PRODUCES}}", string.Join(Environment.NewLine, httpEndpointResponses));
 
             VsLogger.Log($"[SdkHttpEndpointDesigner]: {namespaceHelper.Solution}-{httpApi.DisplayName}.HttpEndpoint.Designer");
 
@@ -67,94 +62,89 @@ namespace ApiStudioIO.CodeGen.CSharpMinimalApiDotNet6.Build
         }
         
 
-        private static List<string> BuildHttpTriggerResponseStatusCodes(HttpApi httpApi)
+        private static List<string> BuildHttpEndpointResponseStatusCodes(HttpApi httpApi)
         {
+            var applicationJson = "\"application/json\"";
             var attributes = new List<string>();
             foreach (var statusCode in httpApi.ResponseStatusCodes)
             {
-                var responseHeader = "ResponseHeader";
-                if (statusCode.Type == "Information") responseHeader += "OnInformation";
-                else if (statusCode.Type == "Success") responseHeader += "OnSuccess";
-                else if (statusCode.Type == "Redirection") responseHeader += "OnRedirection";
-                else if (statusCode.Type == "Client Error") responseHeader += "OnClientError";
-                else if (statusCode.Type == "Server Error") responseHeader += "OnServerError";
-
-                var httpStatus = Enum.GetName(typeof(HttpStatusCode), statusCode.HttpStatus);
+                var httpStatus = Enum.GetName(typeof(HttpStatusCodes), statusCode.HttpStatus);
                 httpStatus = httpStatus != null
-                    ? $"HttpStatusCode.{httpStatus}"
-                    : $"(HttpStatusCode){statusCode.HttpStatus}";
+                    ? $"StatusCodes.{httpStatus}"
+                    : $"{statusCode.HttpStatus}";
 
                 if (httpApi.DataModels != null && statusCode.Type == "Success" && httpApi.DataModels.Count > 0)
                     attributes.Add(
-                        $"\t\t[OpenApiResponseWithBody(statusCode: {httpStatus}, contentType: \"application/json\", bodyType: typeof({httpApi.DataModels?[0].Name}), Summary = \"{statusCode.Description}\", Description = \"{statusCode.Description}\", CustomHeaderType = typeof({responseHeader}))]");
+                        $"\t\t\t\t.Produces<{httpApi.DataModels?[0].Name}>(statusCode: {httpStatus}, contentType: {applicationJson})");
+                        //$"\t\t\t\t[OpenApiResponseWithBody(statusCode: {httpStatus}, contentType: \"application/json\", bodyType: typeof({httpApi.DataModels?[0].Name}), Summary = \"{statusCode.Description}\", Description = \"{statusCode.Description}\", CustomHeaderType = typeof({responseHeader}))]");
                 else if (httpApi.DataModels != null && statusCode.Type == "Success" && httpApi.DataModels.Count == 0)
                     attributes.Add(
-                        $"\t\t[OpenApiResponseWithoutBody(statusCode: {httpStatus}, Summary = \"{statusCode.Description}\", Description = \"{statusCode.Description}\", CustomHeaderType = typeof({responseHeader}))]");
+                        $"\t\t\t\t.Produces(statusCode: {httpStatus}, contentType: {applicationJson})");
+                        //$"\t\t\t\t[OpenApiResponseWithoutBody(statusCode: {httpStatus}, Summary = \"{statusCode.Description}\", Description = \"{statusCode.Description}\", CustomHeaderType = typeof({responseHeader}))]");
                 else
                     attributes.Add(
-                        $"\t\t[OpenApiResponseWithBody(statusCode: {httpStatus}, contentType: \"application/json\", bodyType: typeof(ErrorModel), Summary = \"{statusCode.Description}\", Description = \"{statusCode.Description}\", CustomHeaderType = typeof({responseHeader}))]");
+                        $"\t\t\t\t.Produces<ErrorModel>(statusCode: {httpStatus}, contentType: {applicationJson})");
             }
-
             return attributes;
         }
 
-        private static List<string> BuildHttpTriggerParameters(HttpApi httpApi)
-        {
-            var attributes = new List<string>();
-            foreach (var header in httpApi.RequestHeaders)
-            {
-                var attribute = "\t\t[OpenApiParameter(";
-                attribute += $"name: \"{header.Name}\", ";
-                attribute += "In = ParameterLocation.Header, ";
-                attribute += $"Required = {header.IsRequired.ToString().ToLower()}, ";
-                attribute += "Type = typeof(string), ";
-                attribute += $"Summary = \"{header.Description}\", ";
-                attribute += $"Description = \"{header.Description}\", ";
-                attribute += "Visibility = OpenApiVisibilityType.Important)]";
-                attributes.Add(attribute);
-            }
+        //private static List<string> BuildHttpTriggerParameters(HttpApi httpApi)
+        //{
+        //    var attributes = new List<string>();
+        //    foreach (var header in httpApi.RequestHeaders)
+        //    {
+        //        var attribute = "\t\t[OpenApiParameter(";
+        //        attribute += $"name: \"{header.Name}\", ";
+        //        attribute += "In = ParameterLocation.Header, ";
+        //        attribute += $"Required = {header.IsRequired.ToString().ToLower()}, ";
+        //        attribute += "Type = typeof(string), ";
+        //        attribute += $"Summary = \"{header.Description}\", ";
+        //        attribute += $"Description = \"{header.Description}\", ";
+        //        attribute += "Visibility = OpenApiVisibilityType.Important)]";
+        //        attributes.Add(attribute);
+        //    }
 
-            foreach (var parameter in httpApi.RequestParameters)
-            {
-                var parameterType = parameter.FromType.ConvertOpenApiAttribute();
-                if (parameterType == "ParameterLocation.Body")
-                {
-                    var attribute = "\t\t[OpenApiRequestBody(";
-                    attribute += "contentType: \"application/json\", ";
-                    attribute += $"bodyType: typeof({parameter.DataType}), ";
-                    attribute += $"Required = {parameter.IsRequired.ToString().ToLower()}, ";
-                    attribute += $"Description = \"{parameter.Description}\")]";
-                    attributes.Add(attribute);
-                }
-                else
-                {
-                    var attribute = "\t\t[OpenApiParameter(";
-                    attribute += $"name: \"{parameter.Name.ToAlphaNumeric()}\", ";
-                    attribute += $"In = {parameter.FromType.ConvertOpenApiAttribute()}, ";
-                    attribute += $"Required = {parameter.IsRequired.ToString().ToLower()}, ";
-                    attribute += $"Type = typeof({parameter.DataType}), ";
-                    attribute += $"Summary = \"{parameter.Description}\", ";
-                    attribute += $"Description = \"{parameter.Description}\", ";
-                    attribute += "Visibility = OpenApiVisibilityType.Important)]";
-                    attributes.Add(attribute);
-                }
-            }
+        //    foreach (var parameter in httpApi.RequestParameters)
+        //    {
+        //        var parameterType = parameter.FromType.ConvertOpenApiAttribute();
+        //        if (parameterType == "ParameterLocation.Body")
+        //        {
+        //            var attribute = "\t\t[OpenApiRequestBody(";
+        //            attribute += "contentType: \"application/json\", ";
+        //            attribute += $"bodyType: typeof({parameter.DataType}), ";
+        //            attribute += $"Required = {parameter.IsRequired.ToString().ToLower()}, ";
+        //            attribute += $"Description = \"{parameter.Description}\")]";
+        //            attributes.Add(attribute);
+        //        }
+        //        else
+        //        {
+        //            var attribute = "\t\t[OpenApiParameter(";
+        //            attribute += $"name: \"{parameter.Name.ToAlphaNumeric()}\", ";
+        //            attribute += $"In = {parameter.FromType.ConvertOpenApiAttribute()}, ";
+        //            attribute += $"Required = {parameter.IsRequired.ToString().ToLower()}, ";
+        //            attribute += $"Type = typeof({parameter.DataType}), ";
+        //            attribute += $"Summary = \"{parameter.Description}\", ";
+        //            attribute += $"Description = \"{parameter.Description}\", ";
+        //            attribute += "Visibility = OpenApiVisibilityType.Important)]";
+        //            attributes.Add(attribute);
+        //        }
+        //    }
 
-            return attributes;
-        }
+        //    return attributes;
+        //}
 
-        private static string ConvertOpenApiAttribute(this HttpTypeParameterLocation httpTypeParameterLocation)
-        {
-            switch (httpTypeParameterLocation)
-            {
-                case HttpTypeParameterLocation.Query: return "ParameterLocation.Query";
-                case HttpTypeParameterLocation.Path: return "ParameterLocation.Path";
-                case HttpTypeParameterLocation.Body: return "ParameterLocation.Body";
+        //private static string ConvertOpenApiAttribute(this HttpTypeParameterLocation httpTypeParameterLocation)
+        //{
+        //    switch (httpTypeParameterLocation)
+        //    {
+        //        case HttpTypeParameterLocation.Query: return "ParameterLocation.Query";
+        //        case HttpTypeParameterLocation.Path: return "ParameterLocation.Path";
+        //        case HttpTypeParameterLocation.Body: return "ParameterLocation.Body";
 
-                default:
-                    throw new InvalidEnumArgumentException(nameof(httpTypeParameterLocation),
-                        (int)httpTypeParameterLocation, typeof(HttpTypeParameterLocation));
-            }
-        }
+        //        default:
+        //            throw new InvalidEnumArgumentException(nameof(httpTypeParameterLocation),
+        //                (int)httpTypeParameterLocation, typeof(HttpTypeParameterLocation));
+        //    }
+        //}
     }
 }
