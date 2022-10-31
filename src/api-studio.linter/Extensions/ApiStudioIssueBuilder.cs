9545014c663748e77196d2b2dacc4ed17fa5c269
Andrew Butson <butsona@arbs.io>
2022-10-31 13:34:03 +0100

    adding builder for linter issues

---
+++ src/api-studio.linter/Extensions/ApiStudioIssueBuilder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Andrew Butson.
+// Licensed under the MIT License.
+
+using ApiStudioIO.Common.Models.Linting;
+using ApiStudioIO.Linter.Rules;
+
+namespace ApiStudioIO.Linter.Extensions
+{
+    internal static class ApiStudioIssueBuilder
+    {
+        internal static ApiStudioIssue GetApiStudioIssue(IApiStudioRuleConstants constants, string modelName, string message)
+        {
+            return new ApiStudioIssue()
+            {
+                Rule = $"ApiStudio:{constants.GetRuleType()}.{constants.GetRuleId()}",
+                Severity = $"{constants.GetSeverity()}",
+                Component = $"serviceKey:ApiStudio/{modelName}.ApiStudio",
+                Line = 0,
+                Message = message,
+                Type = $"{constants.GetRuleId()}"
+            };
+ }
+    }
+}
