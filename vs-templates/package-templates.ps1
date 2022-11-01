f2c4c160c28637f92688dbb5a9e86db16eed7187
Andrew Butson <butsona@arbs.io>
2022-11-01 16:37:27 +0100

    name shrink

--- vs-templates/package-templates.ps1
+++ vs-templates/package-templates.ps1
@@ -5,14 +5,14 @@ Get-ChildItem .\ -include "bin","obj",".vs","*.user"  -Recurse -Force | `
         remove-item $_.fullname -Force -Recurse
     }
 
-# Build api-studio.codegen.csharp-azurefunction-dotnet6
+# Build csharp-azurefunction-dotnet6
 $template_path = "..\src\api-studio.package\ProjectTemplates\csharp-azurefunction-dotnet6.zip"
 if (Test-Path $template_path) {
     Write-Host "Removing: $($template_path)" -ForegroundColor DarkMagenta
     Remove-Item $template_path
 }
 Write-Host "Packing: $($template_path)" -ForegroundColor Green
-Compress-Archive -Path .\api-studio.codegen.csharp-azurefunction-dotnet6\* -DestinationPath $template_path
+Compress-Archive -Path .\csharp-azurefunction-dotnet6\* -DestinationPath $template_path
 
 
 # Build api-studio.codegen.csharp-azurefunction-dotnet6
