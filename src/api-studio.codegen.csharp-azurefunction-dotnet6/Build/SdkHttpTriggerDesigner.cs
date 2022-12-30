431df0c781bbb9cab69f1d970e8e6aacf989e5ae
Andrew Butson <butsona@arbs.io>
2022-12-30 10:25:40 +0100

    named parameter nameof(httpApi)

--- src/api-studio.codegen.csharp-azurefunction-dotnet6/Build/SdkHttpTriggerDesigner.cs
+++ src/api-studio.codegen.csharp-azurefunction-dotnet6/Build/SdkHttpTriggerDesigner.cs
@@ -133,7 +133,7 @@ namespace ApiStudioIO.CodeGen.CSharpAzureFunctionDotNet6.Build
                 case SecuritySchemeTypes.None:
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(httpApi.ApiStudio.SecuritySchemeType), "Security Scheme Type is not supported");
+                    throw new ArgumentOutOfRangeException(nameof(httpApi), "Security Scheme Type is not supported");
             }
 
             return attributes;
